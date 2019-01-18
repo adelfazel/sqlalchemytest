5aa2866aba7e3bdf3e5529e19d2d3eef7f0c59f7
Adel Fazel <adel.fazel86@gmail.com>
2019-01-18 15:11:25 +1100

    making database more neat

---
+++ database_update.sql
@@ -0,0 +1,37 @@
+start transaction;
+
+
+CREATE TABLE bookreviews(
+username VARCHAR (50) REFERENCES account(username),
+isbn VARCHAR (15) REFERENCES books(isbn),
+comment varchar (5000) not NULL,
+stars smallint not NULL,
+created_on TIMESTAMP NOT NULL,
+CONSTRAINT username_isbn PRIMARY KEY (username,isbn)
+
+);
+
+
+
+CREATE TABLE account(
+ username VARCHAR (50)  PRIMARY KEY,
+ email VARCHAR (355) UNIQUE NOT NULL,
+ password VARCHAR (40) NOT NULL,
+ created_on TIMESTAMP NOT NULL,
+ last_login TIMESTAMP
+);
+
+
+
+CREATE TABLE books (
+isbn VARCHAR (15) PRIMARY KEY,
+title VARCHAR (400) NOT NULL,
+author VARCHAR (400) NOT NULL,
+year INTEGER NOT NULL
+);
+
+
+
+
+commit;
+
